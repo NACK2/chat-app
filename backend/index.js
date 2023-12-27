b00b01faecfaf0132e6d22e184bbfe7f4a797fca
nick <nicholaskang5@gmail.com>
2023-12-27 15:48:48 -0800

    - Figured out hwo to customize css UI

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.post("/authenticate", async (req, res) => {
     const r = await axios.put(
         'https://api.chatengine.io/users/',
         {username: username, secret: username, first_name: username},
-        {headers: {"private-key": "d98ff472-5f53-4586-b3ef-8ccce7a717ec"}}
+        {headers: {"private-key": "d98ff472-5f53-4586-b3ef-8ccce7a717ec"}} // PROJECT PRIVATE KEY
     )
     return res.status(r.status).json(r.data)
   } catch (e) {
--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,17 +1,38 @@
 import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
 
+import "./ChatsPage.css";
+
 const ChatsPage = (props) => {
     const chatProps = useMultiChatLogic(
-        'd00b15d8-699b-4210-a469-8d4da48dadbd',
+        'd00b15d8-699b-4210-a469-8d4da48dadbd', // PROJECT ID
          props.user.username, 
          props.user.secret
     );
     return (
         <div style={{ height: '100vh'}}>
             <MultiChatSocket {...chatProps} />
-            <MultiChatWindow {...chatProps} style={{ height: '100%'}} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%'}}/>
         </div>
     )
 }
 
 export default ChatsPage
+
+// import { ChatEngine } from 'react-chat-engine';
+
+// const ChatsPage = (props) => {
+// 	return (
+//         <div style = {{height: '100vh'}}>
+//             <ChatEngine
+//                 projectID='d00b15d8-699b-4210-a469-8d4da48dadbd'
+//                 userName={props.user.username}
+//                 userSecret={props.user.secret}
+//                 // Render Custom Components
+//                 height='100vh'
+//                 renderChatList={(props) => {}}
+//             />
+//         </div>
+// 	);
+// }
+
+// export default ChatsPage;
